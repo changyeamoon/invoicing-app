c05df7b5861818e80bc39e795d7895087a6607b5
Chang Yea Moon <changyeamoon@gmail.com>
2020-01-22 17:02:23 -0800

    initial project setup
    - poor mans eslint + prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    bracketSpacing: true,
+    printWidth: 70,
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+}
---
+++ src/Routes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+
+function Routes() {
+  return (
+    <>
+      <Switch></Switch>
+    </>
+  )
+}
